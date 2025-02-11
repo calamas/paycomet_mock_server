84968fd47457fd81622207ea0c984e063f5e4e9d
Claudio Lamas <calamas@taxitronic.com>
2025-02-11 19:42:34 -0300

    Initial commit 2

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ const PORT = process.env.PORT || 443;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 
 // Mock API Response
 app.get("/api/hello", (req, res) => {
-    res.json({ message: "Hello from the mock server!" });
+    res.json({
+        message: "Hello from the mock server!"
+    });
 });
 
 // Set up file upload using Multer
@@ -21,17 +25,61 @@ const storage = multer.diskStorage({
     filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname)),
 });
 
-const upload = multer({ storage });
+const upload = multer({
+    storage
+});
 
 app.post("/api/upload", upload.single("file"), (req, res) => {
-    if (!req.file) return res.status(400).json({ error: "No file uploaded" });
-    res.json({ message: "File uploaded successfully!", filename: req.file.filename });
+    if (!req.file) return res.status(400).json({
+        error: "No file uploaded"
+    });
+    res.json({
+        message: "File uploaded successfully!",
+        filename: req.file.filename
+    });
 });
 
 // Default route
-app.get("/", (req, res) => {
-    res.send("Mock server running!");
+app.post("/payments", (req, res) => {
+    const {
+        payment: {
+            amount,
+            currency,
+            methodId,
+            order,
+            originalIp,
+            secure,
+            termina,
+            idUser,
+            tokenUser,
+            productDescription
+        }
+    } = req.body;
+
+    res.json({
+        amount,
+        currency,
+        methodId,
+        order,
+        challengeUrl: "https://api.paycomet.com/gateway/emv3ds_challenge.php?trackId=294997301&commData=ZFNoU2RtdytPRkk5UEVwcVl5UXpKamQyWUVaRldqeHJiVHBRYkVsUVpTTTlKRkZwTldaMFlFTjJNaVZoYmtCeE0zUmpkREZFTWpBM2VHWlFlVHRH&lang=es",
+        idUser,
+        tokenUser,
+        cardCountry: "ESP",
+        errorCode: 0
+    });
 });
+
+
+
+
+app.get("/api/hello", (req, res) => {
+    res.json({
+        message: "Hello from the mock server!"
+    });
+});
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
