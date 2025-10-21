8b735728404cd05e7295ae279b5706a54a32903e
Claudio Lamas <calamas@taxitronic.com>
2025-10-21 07:00:39 -0300

    add transaction info endpoint to retrieve payment details

--- server.js
+++ server.js
@@ -191,6 +191,97 @@ app.post("/payments/:transactionId/refund", (req, res) => {
   }
 });
 
+app.post("/payments/:transactionId/info", (req, res) => {
+  const { transactionId } = req.params;
+
+  res.json({
+    errorCode: 0,
+    payment: {
+      amount: "200",
+      amountDisplay: "2,00 €",
+      amountEur: "2.00",
+      amountEurDisplay: "2,00 €",
+      authCode: "709604/267317868823855755525078875212",
+      bicCode: "PAYTPVMMXXX",
+      cardBrand: "VISA",
+      cardCategory: "GOLD",
+      cardCountry: "FR",
+      cardType: "CREDIT",
+      costumerCountry: "ES",
+      currency: "EUR",
+      dcc: 0,
+      errorCode: 0,
+      errorDescription: "Sin error",
+      feeEuro: "0",
+      feePercent: "0",
+      issuerBank: "BPCE",
+      merchantData: [
+        "eyJjdXN0b21lciI6eyJpZCI6ImVhNjJkZTJlLTZhMzEtNDc1YS04N2FhLTg0MTAxMmRkZmJhZiIsIm5hbWUiOiJWZXJvIiwic3VybmFtZSI6IlN0YWdpbmcifX0=",
+      ],
+      methodId: "1",
+      operationId: 350698157,
+      operationName: "Autorización",
+      operationType: 1,
+      order: "1647d545-20ab-477b-9ffb-f09a518013cb",
+      originalIp: "90.162.81.13",
+      pan: "455685******2403",
+      paycometId: "350741409",
+      response: "OK",
+      secure: 0,
+      settlementDate: "20251020170540",
+      state: 1,
+      stateName: "Completado",
+      terminal: 16785,
+      terminalCurrency: "EUR",
+      terminalName: "www.pidetaxi.es - BANKSTORE (TEST)",
+      timestamp: "20251020170540",
+      user: "ZHr4iAvV",
+      history: [
+        {
+          amount: "200",
+          amountDisplay: "2,00 €",
+          amountEur: "2.00",
+          amountEurDisplay: "2,00 €",
+          authCode: "709604/267317868823855755525078875212",
+          bicCode: "PAYTPVMMXXX",
+          cardBrand: "VISA",
+          cardCategory: "GOLD",
+          cardCountry: "FR",
+          cardType: "CREDIT",
+          costumerCountry: "ES",
+          currency: "EUR",
+          dcc: 0,
+          errorCode: 0,
+          errorDescription: "Sin error",
+          feeEuro: "0",
+          feePercent: "0",
+          issuerBank: "BPCE",
+          methodId: "1",
+          operationId: 350698157,
+          operationName: "Autorización",
+          operationType: 1,
+          order: "1647d545-20ab-477b-9ffb-f09a518013cb",
+          originalIp: "90.162.81.13",
+          pan: "455685******2403",
+          paycometId: "350741409",
+          productDescription:
+            "Trip:cc4cb318-b002-43c1-b2f1-64eb63ff743d,dispId:1025784,Comp:RT Takasi ,App:Pidetaxi APP/veronicalamas@gmail.com",
+          response: "OK",
+          secure: 0,
+          settlementDate: "20251020170540",
+          state: 1,
+          stateName: "Completado",
+          terminal: 16785,
+          terminalCurrency: "EUR",
+          terminalName: "www.pidetaxi.es - BANKSTORE (TEST)",
+          timestamp: "20251020170540",
+          user: "ZHr4iAvV",
+        },
+      ],
+    },
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
